ed315840cd8eb8b096644e427470009f2314f8c0
sknabirasool <sknabirasool104@gmail.com>
2022-06-02 18:59:17 +0530

    first commit

---
+++ WebPortal_02/Pages/admin/login.cshtml.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace WebPortal_02.Pages.admin
+{
+    public class loginModel : PageModel
+    {
+       public logininfo log = new logininfo();
+
+        public string ErrorMessage = "";
+        public string SuccessMessage = "";
+        public void OnGet()
+        {
+            string EMAIL = Request.Query["email"];
+            string PASSWORD = Request.Query["password"];                            
+
+            try
+            {
+                String con = "Data Source=.;Initial Catalog=Registration_db;Integrated Security=True";
+
+                using (SqlConnection cs = new SqlConnection(con))
+                {
+                    cs.Open();
+                    string sqlquery = "Select * from tbl_users where email=@email and password=@password";
+                    using (SqlCommand command = new SqlCommand(sqlquery, cs))
+                    {
+                        command.Parameters.AddWithValue("@email",EMAIL);
+                        command.Parameters.AddWithValue("@password",PASSWORD);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            reader.Read();
+                            //int count = reader.FieldCount();
+                            if (reader.HasRows)
+                            {
+                                Response.Redirect("welcome");
+                            }
+                            else
+                            {
+                                ErrorMessage = "Invalid Login Details";
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return;
+            }
+
+        }
+
+        public void OnPost()
+        {
+            log.email = Request.Form["email"];
+            log.password = Request.Form["password"];
+        }
+
+    }
+
+    public class logininfo
+    {
+        public string email;
+        public string password;
+
+    }
+
+
+
+
+
+}
---
+++ WebPortal_02/Pages/admin/welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebPortal_02.Pages.admin
+{
+    public class welcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
